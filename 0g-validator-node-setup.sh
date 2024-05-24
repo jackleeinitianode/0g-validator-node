08f0e6e8356ed2183e5a2838f78f09c64b4270e0
jackleeinitianode <jackleeinitianode@gmail.com>
2024-05-24 13:12:49 +0800

    Update 0g-validator-node-setup.sh

--- 0g-validator-node-setup.sh
+++ 0g-validator-node-setup.sh
@@ -39,6 +39,72 @@ function clone_script_0gvalidator(){
 	cd 0g-chain
 	make install
 	0gchaind version
+	
+	cd $HOME
+	0gchaind config chain-id $CHAIN_ID
+	0gchaind config node tcp://localhost:$RPC_PORT
+	0gchaind config keyring-backend os
+	0gchaind init $MONIKER --chain-id $CHAIN_ID
+	
+
+	# download genesis
+	wget -P ~/.0gchain/config https://github.com/0glabs/0g-chain/releases/download/v0.1.0/genesis.json
+
+	# Update addrbook
+	curl -o $HOME/.0gchain/config/addrbook.json https://zerog.snapshot.nodebrand.xyz/addrbook.json
+
+	SEEDS="c4d619f6088cb0b24b4ab43a0510bf9251ab5d7f@54.241.167.190:26656,44d11d4ba92a01b520923f51632d2450984d5886@54.176.175.48:26656,f2693dd86766b5bf8fd6ab87e2e970d564d20aff@54.193.250.204:26656,f878d40c538c8c23653a5b70f615f8dccec6fb9f@54.215.187.94:26656, 41143f378016a05e1fa4fa8aa028035a82761b48@154.12.235.67:46656" && \
+	sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.0gchain/config/config.toml
+
+
+	EXTERNAL_IP=$(wget -qO- eth0.me) \
+	PROXY_APP_PORT=36658 \
+	P2P_PORT=38657 \
+	PPROF_PORT=7060 \
+	API_PORT=2317 \
+	GRPC_PORT=10090 \
+	GRPC_WEB_PORT=10091
+
+	sed -i \
+		-e "s/\(proxy_app = \"tcp:\/\/\)\([^:]*\):\([0-9]*\).*/\1\2:$PROXY_APP_PORT\"/" \
+		-e "s/\(laddr = \"tcp:\/\/\)\([^:]*\):\([0-9]*\).*/\1\2:$RPC_PORT\"/" \
+		-e "s/\(pprof_laddr = \"\)\([^:]*\):\([0-9]*\).*/\1localhost:$PPROF_PORT\"/" \
+		-e "/\[p2p\]/,/^\[/{s/\(laddr = \"tcp:\/\/\)\([^:]*\):\([0-9]*\).*/\1\2:$P2P_PORT\"/}" \
+		-e "/\[p2p\]/,/^\[/{s/\(external_address = \"\)\([^:]*\):\([0-9]*\).*/\1${EXTERNAL_IP}:$P2P_PORT\"/; t; s/\(external_address = \"\).*/\1${EXTERNAL_IP}:$P2P_PORT\"/}" \
+		$HOME/.0gchain/config/config.toml
+	sed -i \
+	    -e "/\[api\]/,/^\[/{s/\(address = \"tcp:\/\/\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$API_PORT\4/}" \
+	    -e "/\[grpc\]/,/^\[/{s/\(address = \"\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$GRPC_PORT\4/}" \
+	    -e "/\[grpc-web\]/,/^\[/{s/\(address = \"\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$GRPC_WEB_PORT\4/}" $HOME/.0gchain/config/app.toml
+	sed -i "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0ua0gi\"/" $HOME/.0gchain/config/app.toml
+	sed -i "s/^indexer *=.*/indexer = \"kv\"/" $HOME/.0gchain/config/config.toml
+	sed -i -e 's/address = "127.0.0.1:9545"/address = "0.0.0.0:9545"/' -e 's/ws-address = "127.0.0.1:9546"/ws-address = "0.0.0.0:9546"/' $HOME/.0gchain/config/app.toml
+
+	# Update Peers
+	curl -o $HOME/update_0g_peers.sh https://zerog.snapshot.nodebrand.xyz/update_0g_peers.sh
+	chmod +x $HOME/update_0g_peers.sh
+	$HOME/update_0g_peers.sh
+	# update addrbook
+	curl -o $HOME/.0gchain/config/addrbook.json https://zerog.snapshot.nodebrand.xyz/addrbook.json
+
+	# download snapshot
+	sudo systemctl stop ogd
+	cp $HOME/.0gchain/data/priv_validator_state.json $HOME/.0gchain/priv_validator_state.json.backup
+	0gchaind tendermint unsafe-reset-all --home $HOME/.0gchain --keep-addr-book
+	curl https://snapshots-testnet.nodejumper.io/0g-testnet/0g-testnet_latest.tar.lz4 | sudo lz4 -dc - | sudo tar -xf - -C $HOME/.0gchain
+	mv $HOME/.0gchain/priv_validator_state.json.backup $HOME/.0gchain/data/priv_validator_state.json
+	
+	
+	PORT=$(grep -A 3 "\[rpc\]" $HOME/.0gchain/config/config.toml | egrep -o ":[0-9]+") && PORT=${PORT#:} && echo $PORT
+	sudo ufw allow $PORT
+	sudo ufw reload
+	sed -i '/\[rpc\]/,/\[/{s/^laddr = "tcp:\/\/127\.0\.0\.1:/laddr = "tcp:\/\/0.0.0.0:/}' $HOME/.0gchain/config/config.toml
+	sudo systemctl restart ogd
+	curl $RPC/status
+	echo $RPC
+	
+	
+
 }
 
 
@@ -81,4 +147,3 @@ function menu() {
 
 # Call the menu function
 menu
-
